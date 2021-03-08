a245951c0ff29e13cf3d5175ef1e84b147827744
seunghun yang <yabby1997@gmail.com>
2021-03-08 15:38:21 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Seunghun Yang on 2021/03/08.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
